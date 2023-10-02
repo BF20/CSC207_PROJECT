690cf9284c0b79c5da3abd4ec2020421a6089db7
BF10 <benjaminfitzgerald2000@gmail.com>
2023-10-02 17:36:59 -0400

    some general structure

---
+++ Habit Builder Project/src/DataReader.java
@@ -0,0 +1,13 @@
+public class DataReader {
+}
+
+ /* The idea here is that we have a csv file of 8 fields:
+ * user ("string")
+ * habit ("string")
+ * habit_type (a string which is an element of the set of valid habit types)
+ * habit_start_time (an instance of the java LocalDateTime class)
+ * habit_end_time (an instance of the java LocalDateTime class)
+ * periodic (a bool which represents whether the habit is periodic.)
+ * number_of_periods (how many times the habit repeats). It must be greater than 1.
+ * period_separation (an instance of the java LocalDateTime class. It is the distance between the periods).
+ * */
