f2bea917c36eb3f98519c12c91bc0052de626b62
amcwong <andrew.wong8@icloud.com>
2023-12-03 14:31:14 -0500

    Switch names for users

--- Habit Builder Project/src/app/Main.java
+++ Habit Builder Project/src/app/Main.java
@@ -52,8 +52,8 @@ public class Main {
 
         // Buttons to switch views
         mainAppView.addSwitchButton("Switch to Bob", () -> viewManagerModel.setActiveView("Bob"));
-        mainAppView.addSwitchButton("Switch to User 2", () -> viewManagerModel.setActiveView("User2"));
-        mainAppView.addSwitchButton("Switch to User 3", () -> viewManagerModel.setActiveView("User3"));
+        mainAppView.addSwitchButton("Switch to Alice", () -> viewManagerModel.setActiveView("Alice"));
+        mainAppView.addSwitchButton("Switch to Charile", () -> viewManagerModel.setActiveView("Charile"));
 
         mainAppView.display();
     }
