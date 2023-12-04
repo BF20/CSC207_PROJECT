52261e9d5510b4462d223f41a970f4dd7b74d995
amcwong <andrew.wong8@icloud.com>
2023-12-04 15:57:30 -0500

    Yi datavisualization legacy commit

---
+++ Habit Builder Project/src/entity/GroupGoal.java
@@ -0,0 +1,18 @@
+package entity;
+
+public class GroupGoal {
+    private int goalHours;
+
+    public GroupGoal(int goalHours) {
+        this.goalHours = goalHours;
+    }
+
+    // Getter and Setter
+    public int getGoalHours() {
+        return goalHours;
+    }
+
+    public void setGoalHours(int goalHours) {
+        this.goalHours = goalHours;
+    }
+}
---
+++ Habit Builder Project/src/use_case/group_goal/GroupGoalOutputData.java
@@ -0,0 +1,14 @@
+package use_case.group_goal;
+
+public class GroupGoalOutputData {
+    private final int goalHours;
+
+    public GroupGoalOutputData(int goalHours) {
+        this.goalHours = goalHours;
+    }
+
+    public int getGoalHours() {
+        return goalHours;
+    }
+}
+
